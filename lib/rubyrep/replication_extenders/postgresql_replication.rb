ManageIQ/rubyrep

commit af43a45a68bb6ef153e458b37563a90b3fb3de29

Hack an attempt at Rails 5 compatibility

lib/rubyrep/replication_extenders/postgresql_replication.rb
@@ -15,6 +15,11 @@ module RR
       end
       private :key_clause
 
+      TABLE_PLACEHOLDER = "'table'placeholder'"
+      def expand_conditional_clause(cond, relation_name)
+        RR::ProxyConnection.expand_conditional_clause(cond, TABLE_PLACEHOLDER).gsub(TABLE_PLACEHOLDER, relation_name)
+      end
+
       # Returns the filter clause that is used to filter rows from the trigger.
       # * +conditions+: ActiveRecord style conditions, optionally preceded by the keys
       #   :new or :old, to describe on which side to apply the conditions.
@@ -23,7 +28,7 @@ module RR
         if conditions.kind_of?(Hash) && (conditions.has_key?(:new) || conditions.has_key?(:old))
           clause = conditions.map do |trigger_var, c|
             t = trigger_var.to_s.upcase
-            Class.new(ActiveRecord::Base).send(:sanitize_sql_for_conditions, c, t).gsub(/^"#{t}"/, t)
+            expand_conditional_clause(c, t)
           end.join(" AND ")
         else
           clause = conditions

lib/rubyrep/proxy_connection.rb
@@ -318,6 +318,21 @@ module RR
     end
     private :row_condition
 
+    def self.expand_conditional_clause(cond, relation_name)
+      cond = cond.stringify_keys
+
+      klass = Class.new(ActiveRecord::Base) { self.table_name = relation_name; singleton_class.send(:define_method, :load_schema) { } }
+
+      return klass.send(:sanitize_sql_for_conditions, cond) unless cond.is_a?(Hash)
+
+      cond = klass.predicate_builder.resolve_column_aliases(cond)
+      cond = klass.send(:expand_hash_conditions_for_aggregates, cond)
+
+      klass.predicate_builder.build_from_hash(cond).map { |b|
+        klass.connection.visitor.compile b
+      }.join(' AND ')
+    end
+
     # Returns an SQL query string for the given +table+ based on the provided +options+.
     # +options+ is a hash that can contain any of the following:
     #   * :+from+: nil OR the hash of primary key => value pairs designating the start of the selection
@@ -358,7 +373,7 @@ module RR
         end
       end
       if options[:conditions]
-        query << ' and (not (' << Class.new(ActiveRecord::Base).send(:sanitize_sql_for_conditions, options[:conditions], quote_table_name(table)) << '))'
+        query << ' and (not (' << ProxyConnection.expand_conditional_clause(options[:conditions], table) << '))'
       end
       query << " order by #{quote_key_list(table)}"
 

lib/rubyrep/table_spec_resolver.rb
@@ -88,7 +88,7 @@ module RR
         case table_spec
         when /^\/.*\/$/ # matches e. g. '/^user/'
           table_spec = table_spec.sub(/^\/(.*)\/$/,'\1') # remove leading and trailing slash
-          matching_tables = tables(:left).grep(Regexp.new(table_spec, Regexp::IGNORECASE, 'U'))
+          matching_tables = tables(:left).grep(Regexp.new(table_spec, Regexp::IGNORECASE))
           matching_tables.each do |table|
             if !verify or tables(:right).include? table
               table_pairs << {:left => table, :right => table}

lib/rubyrep/type_casting_cursor.rb
@@ -24,7 +24,7 @@ module RR
     # Reads the next row from the original cursor and returns the row with the type casted row values.
     def next_row
       row = org_cursor.next_row
-      row.each {|column, value| row[column] = columns[column].type_cast_from_database value}
+      row.each {|column, value| row[column] = value}
       row
     end    
   end

commit aabf49ba3c14dc7708c0ba18446c043af07e7ea1

Remove the quotes from the trigger variable names OLD and NEW
This was a regression introduced in af43a45a68bb6ef153e458b37563a90b3fb3de29

lib/rubyrep/replication_extenders/postgresql_replication.rb
@@ -17,7 +17,7 @@ module RR
 
       TABLE_PLACEHOLDER = "'table'placeholder'"
       def expand_conditional_clause(cond, relation_name)
-        RR::ProxyConnection.expand_conditional_clause(cond, TABLE_PLACEHOLDER).gsub(TABLE_PLACEHOLDER, relation_name)
+        RR::ProxyConnection.expand_conditional_clause(cond, TABLE_PLACEHOLDER).gsub("\"#{TABLE_PLACEHOLDER}\"", relation_name)
       end
 
       # Returns the filter clause that is used to filter rows from the trigger.